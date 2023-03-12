f49703aa7a6d4458cbdd5248779666b61c9ae653
Ana Escobar <anaescobar8@gmail.com>
2023-03-13 00:01:25 +0100

    añadidos enunciados

---
+++ ejercicios/ficheros/enunciados_varios.py
@@ -0,0 +1,19 @@
+# 1.  Crea un programa que lea un archivo de texto y cuente el número de líneas que contiene
+
+# 2. Crea un programa que lea un archivo de texto y muestre su contenido por pantalla
+
+# 3. Crea un programa que escriba un mensaje en un archivo de texto
+
+# 4. Crea un programa que lea un archivo de texto y elimine todas las líneas en blanco
+
+# 5. Crea un programa que lea un archivo de texto y busque una palabra específica en él
+
+# 6. Crea un programa que copie el contenido de un archivo de texto a otro archivo
+
+# 7. Crea un programa que lea un archivo de texto y reemplace una palabra específica por otra
+
+# 8. Crea un programa que lea un archivo de texto y muestre las líneas que contienen una palabra específica
+
+# 9. Crea un programa que lea un archivo de texto y cuente el número de veces que aparece una palabra específica
+
+# 10. Crea un programa que lea un archivo de texto y elimine todas las palabras repetidas
---
+++ ejercicios/funciones/enunciados_varios.py
@@ -0,0 +1,21 @@
+import math
+
+# 1. Crea una función que calcule el área de un círculo a partir de su radio
+
+# 2. Crea una función que calcule la suma de dos números
+
+# 3. Crea una función que calcule el área de un triángulo a partir de su base y altura
+
+# 4. Crea una función que calcule el máximo entre dos números
+
+# 5. Crea una función que calcule el factorial de un número
+
+# 6. Crea una función que verifique si un número es par o impar
+
+# 7. Crea una función que calcule el valor absoluto de un número
+
+# 8. Crea una función que verifique si un número es primo
+
+# 9. Crea una función que calcule el promedio de una lista de números
+
+# 10. Crea una función que calcule el área de un rectángulo a partir de su base y altura
